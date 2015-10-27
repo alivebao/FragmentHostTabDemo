b9ef5f9fd3b840526a2de29b809d196b8d4f378d
alivebao <641572831@qq.com>
2015-10-27 16:11:32 +0800

    demo

---
+++ README.md
@@ -0,0 +1,2 @@
+Demo of FrgmentTabHost
+[DemoPic](Demo.jpeg)
---
+++ app/src/main/java/com/miao/administrator/fragementtabhostdemo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.miao.administrator.fragementtabhostdemo;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost mTabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mTabHost = (FragmentTabHost) findViewById(R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        mTabHost.addTab(mTabHost.newTabSpec("0").setIndicator("News"), FragmentNews.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("1").setIndicator("Music"), FragmentMusic.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("2").setIndicator("Others"), FragmentOthers.class, null);
+//        hide devideline
+//        mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
